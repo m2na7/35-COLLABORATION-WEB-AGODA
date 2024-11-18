7b70af69462f4aa2d8a51694d3891304ff3adfb4
Parkchaeyeon <ccyy1029@naver.com>
2024-11-19 02:05:11 +0900

    feat: 로고 클릭 / 이전버튼 클릭 시 navigate 함수 분리하기

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,22 +18,31 @@ interface HeaderProps {
 const Header = ({ isPrevButton = false }: HeaderProps) => {
   const navigate = useNavigate();
 
+  const handleLogoClick = () => {
+    navigate(routePath.HOME);
+  };
+
+  const handleBackClick = () => {
+    if (window.history.length > 2) {
+      navigate(-1);
+    } else {
+      navigate(routePath.HOME);
+    }
+  };
+
   return (
     <>
       <div css={headerLayout}>
         {isPrevButton ? (
           <section css={headerLeftSectionWrapper}>
-            <IcBack css={headerLeftIconStyle} onClick={() => navigate(-1)} />
+            <IcBack css={headerLeftIconStyle} onClick={handleBackClick} />
           </section>
         ) : (
           <div></div>
         )}
 
         <section css={headerCenterSectionWrapper}>
-          <IcLogo
-            css={headerLogoStyle}
-            onClick={() => navigate(routePath.HOME)}
-          />
+          <IcLogo css={headerLogoStyle} onClick={handleLogoClick} />
         </section>
 
         <section css={headerRightSectionWrapper}>
