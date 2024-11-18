33312f89d78cc45578a93742eb363641f34db3f8
Parkchaeyeon <ccyy1029@naver.com>
2024-11-19 01:54:56 +0900

    feat: Header 컴포넌트 만들기

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,46 @@
 import { IcBack, IcCart, IcLogo, IcMenu } from "@assets/svg";
+import {
+  headerCenterSectionWrapper,
+  headerLayout,
+  headerLeftIconStyle,
+  headerLeftSectionWrapper,
+  headerLogoStyle,
+  headerRightIconStyle,
+  headerRightSectionWrapper,
+} from "@components/Header/Header.style";
+import routePath from "@routes/routePath";
+import { useNavigate } from "react-router-dom";
 
-const Header = () => {
+interface HeaderProps {
+  isPrevButton?: boolean;
+}
+
+const Header = ({ isPrevButton = false }: HeaderProps) => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <IcBack />
-      <IcLogo />
-      <IcCart />
-      <IcMenu />
+      <div css={headerLayout}>
+        {isPrevButton ? (
+          <section css={headerLeftSectionWrapper}>
+            <IcBack css={headerLeftIconStyle} />
+          </section>
+        ) : (
+          <div></div>
+        )}
+
+        <section
+          css={headerCenterSectionWrapper}
+          onClick={() => navigate(routePath.HOME)}
+        >
+          <IcLogo css={headerLogoStyle} />
+        </section>
+
+        <section css={headerRightSectionWrapper}>
+          <IcCart css={headerRightIconStyle} />
+          <IcMenu css={headerRightIconStyle} />
+        </section>
+      </div>
     </>
   );
 };
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,11 @@
+import Header from "@components/Header/Header";
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <Header />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
