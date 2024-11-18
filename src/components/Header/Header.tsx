a116fabc5de5a37db409d836c85430afcfac517d
Parkchaeyeon <ccyy1029@naver.com>
2024-11-19 01:04:53 +0900

    chore: 필요한 svg 세팅 / Header 컴포넌트 세팅

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import { IcBack, IcCart, IcLogo, IcMenu } from "@assets/svg";
+
+const Header = () => {
+  return (
+    <>
+      <IcBack />
+      <IcLogo />
+      <IcCart />
+      <IcMenu />
+    </>
+  );
+};
+
+export default Header;
