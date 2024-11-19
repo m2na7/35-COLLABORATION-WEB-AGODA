5898f5e2f8d8294b351f9d94dc6d78c3b8ef225f
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-20 01:00:25 +0900

    refactor: add React-Slick, add thumbnail Props

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,9 @@ import Carousel from "@components/Carousel/Carousel";
 const App: React.FC = () => {
   const router = createBrowserRouter([...pageRoutes]);
 
-  const thumbnail = "https://img.youtube.com/vi/YwC0m0XaD2E/0.jpg";
-
   return (
     <ThemeProvider theme={theme}>
       <Global styles={GlobalStyle} />
-      <Carousel thumbnail={thumbnail} />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
