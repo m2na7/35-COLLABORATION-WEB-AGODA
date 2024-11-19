9cb61cc8ff426a09946f768f20765da22d24a7bb
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-20 01:57:50 +0900

    feat: 아이콘 SVG 및 컴포넌트 추가

---
+++ src/assets/svg/IcEmptyHeart.tsx
@@ -0,0 +1,15 @@
+import type { SVGProps } from "react";
+const SvgIcEmptyHeart = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 16 16"
+    {...props}
+  >
+    <path
+      fill="#878586"
+      d="M8.001 3.91a3.617 3.617 0 0 1 4.945.136 3.553 3.553 0 0 1 .142 4.9L8 14.001 2.914 8.947a3.553 3.553 0 0 1 .143-4.904A3.617 3.617 0 0 1 8 3.91m4.095.977a2.41 2.41 0 0 0-3.294-.09l-.8.712-.802-.712a2.41 2.41 0 0 0-3.284.079 2.368 2.368 0 0 0-.127 3.256L8 12.316l4.212-4.184a2.37 2.37 0 0 0-.116-3.245"
+    />
+  </svg>
+);
+export default SvgIcEmptyHeart;
