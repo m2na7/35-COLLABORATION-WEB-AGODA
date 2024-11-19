65b1d0570850b6fe26efac22f087f72a0229007a
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-20 01:57:50 +0900

    feat: 아이콘 SVG 및 컴포넌트 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,6 +1,10 @@
-export { default as IcBack } from "./IcBack";
-export { default as IcCart } from "./IcCart";
-export { default as IcLogo } from "./IcLogo";
-export { default as IcMenu } from "./IcMenu";
-export { default as IcStar } from "./IcStar";
-export { default as IcStarBlue } from "./IcStarBlue";
+export { default as IcBack } from './IcBack';
+export { default as IcCart } from './IcCart';
+export { default as IcLogo } from './IcLogo';
+export { default as IcMenu } from './IcMenu';
+export { default as IcEmptyHeart } from './IcEmptyHeart';
+export { default as IcFillHeart } from './IcFillHeart';
+export { default as IcMap } from './IcMap';
+export { default as IcStar } from './IcStar';
+export { default as IcStarBlue } from './IcStarBlue';
+export { default as TagLightred } from './TagLightred';
