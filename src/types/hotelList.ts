c29021355a0da9afd01e237b76758cd818ee5353
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-20 01:56:57 +0900

    feat: 호텔 리스트 타입 및 목 데이터 추가

---
+++ src/types/hotelList.ts
@@ -0,0 +1,14 @@
+export interface HotelList {
+  hotelId: number;
+  hotelName: string;
+  star: number;
+  hotelDistrict: string;
+  rating: number;
+  reviewCount: number;
+  originalPrice: number;
+  discountPrice: number;
+  taxPrice: number;
+  isLiked: boolean;
+  isQuarterDiscount: boolean;
+  hotelImage?: string;
+}
