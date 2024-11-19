929c87ae02e1b043b78c816b065c44ae49a8f60c
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-20 01:58:43 +0900

    feat: 호텔 리스트 카드 컴포넌트 추가
    - 호텔리스트 페이지내에서 카드출력

--- src/pages/HotelListPage/HotelListPage.tsx
+++ src/pages/HotelListPage/HotelListPage.tsx
@@ -1,5 +1,15 @@
+import { HOTEL_LIST_DATA } from '@utils/mocks/hotelListData';
+import HotelListCard from './components/HotelListCard';
+import { cardContainer } from './HotelListPage.style';
+
 const HotelListPage = () => {
-  return <div>HotelListPage</div>;
+  return (
+    <section css={cardContainer}>
+      {HOTEL_LIST_DATA.data.map((hotel) => (
+        <HotelListCard key={hotel.hotelId} hotel={hotel} />
+      ))}
+    </section>
+  );
 };
 
 export default HotelListPage;
---
+++ src/pages/HotelListPage/components/HotelListCard.style.ts
@@ -0,0 +1,119 @@
+import { css, Theme } from '@emotion/react';
+
+const hotelCardWrapper = (theme: Theme) => css`
+  display: flex;
+  width: 95%;
+  height: 18rem;
+
+  border-radius: 16px;
+  background-color: ${theme.color.white};
+`;
+
+const hotelInfoWrapper = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  position: relative;
+  flex-grow: 1;
+  padding: 0.9rem 1rem 0.8rem 0.8rem;
+`;
+
+const hotelDescriptionWrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+`;
+
+const hotelCardImage = css`
+  width: 14.4rem;
+  height: 18rem;
+  object-fit: cover;
+  border-radius: 16px 0 0 16px;
+`;
+
+const hotelTitle = (theme: Theme) => css`
+  ${theme.font.title4_b_16}
+`;
+
+const hotelDetailWrapper = css`
+  display: flex;
+  gap: 1.2rem;
+`;
+
+const hotelLocation = (theme: Theme) => css`
+  display: flex;
+  gap: 0.2rem;
+  ${theme.font.body4_m_12}
+`;
+
+const hotelRating = (theme: Theme) => css`
+  ${theme.font.button2_eb_12}
+  color: ${theme.color.blue300};
+`;
+
+const hotelReviewWrapper = (theme: Theme) => css`
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+  ${theme.font.body4_m_12}
+`;
+
+const hotelReview = (theme: Theme) => css`
+  ${theme.font.detail3_m_12}
+  color: ${theme.color.gray600};
+`;
+
+const hotelHeartIcon = css`
+  position: absolute;
+  top: 0.9rem;
+  right: 1rem;
+`;
+
+const hotelPriceWrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  text-align: right;
+`;
+
+const hotelOriginalPrice = (theme: Theme) => css`
+  ${theme.font.body4_m_12}
+  color: ${theme.color.gray600};
+  text-decoration: line-through;
+`;
+
+const hotelDiscountPrice = (theme: Theme) => css`
+  ${theme.font.title2_sb_20}
+`;
+
+const hotelTaxPrice = (theme: Theme) => css`
+  ${theme.font.body4_m_12}
+  color: ${theme.color.gray600};
+`;
+
+const hotelTagPrice = (theme: Theme) => css`
+  display: flex;
+  align-items: end;
+  justify-content: end;
+  gap: 0.8rem;
+  color: ${theme.color.red200};
+`;
+
+export {
+  hotelCardWrapper,
+  hotelInfoWrapper,
+  hotelDescriptionWrapper,
+  hotelCardImage,
+  hotelTitle,
+  hotelDetailWrapper,
+  hotelLocation,
+  hotelRating,
+  hotelReviewWrapper,
+  hotelReview,
+  hotelHeartIcon,
+  hotelPriceWrapper,
+  hotelOriginalPrice,
+  hotelDiscountPrice,
+  hotelTaxPrice,
+  hotelTagPrice,
+};
