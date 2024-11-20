0abba395d4f64bfd32c006c1e57a5f022b95cfdd
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-20 22:39:48 +0900

    refactor: 이미지 배열로 수정

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -4,20 +4,11 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import * as styles from "./Carousel.styles";
 
-//고정될 사진들
-import fix1 from "@/assets/img/fix-1.jpg";
-import fix2 from "@/assets/img/fix-2.jpg";
-import fix3 from "@/assets/img/fix-3.jpg";
-import fix4 from "@/assets/img/fix-4.jpg";
-
-// 썸네일 Props
 interface CarouselProps {
-  thumbnail: string;
+  images: string[];
 }
 
-const Carousel = ({ thumbnail }: CarouselProps) => {
-  const images = [thumbnail, fix1, fix2, fix3, fix4];
-
+const Carousel = ({ images }: CarouselProps) => {
   const sliderRef = useRef<Slider>(null);
 
   const settings = {
@@ -26,7 +17,7 @@ const Carousel = ({ thumbnail }: CarouselProps) => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
+    arrows: false,
   };
 
   const handleWheel = (e: React.WheelEvent) => {
