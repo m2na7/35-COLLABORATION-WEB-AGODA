e598124c9b5995c090c30269088d5afbefa657cb
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-21 02:31:17 +0900

    feat: 호텔 리스트 페이지 구현

--- src/pages/HotelListPage/HotelListPage.style.ts
+++ src/pages/HotelListPage/HotelListPage.style.ts
@@ -1,15 +1,23 @@
-import { Theme, css } from '@emotion/react';
+import { css } from '@emotion/react';
+import theme from '@styles/theme';
 
-const cardContainer = (theme: Theme) => css`
+const hotelListHeaderContainer = css`
+  display: grid;
+  gap: 1rem;
+  padding: 1rem 1.2rem 1.2rem 1.2rem;
+`;
+
+const hotelListWrapper = css`
+  padding: 0.7rem 0;
+  background-color: ${theme.color.gray200};
+`;
+
+const cardWrapper = css`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 0 auto;
-
-  width: 100%;
   gap: 0.8rem;
-  background-color: ${theme.color.gray200};
 `;
 
-export { cardContainer };
+export { hotelListHeaderContainer, hotelListWrapper, cardWrapper };
--- src/pages/HotelListPage/HotelListPage.tsx
+++ src/pages/HotelListPage/HotelListPage.tsx
@@ -1,14 +1,37 @@
 import { HOTEL_LIST_DATA } from '@utils/mocks/hotelListData';
 import HotelListCard from './components/HotelListCard';
-import { cardContainer } from './HotelListPage.style';
+import {
+  hotelListHeaderContainer,
+  cardWrapper,
+  hotelListWrapper,
+} from './HotelListPage.style';
+import Header from '@components/Header/Header';
+import SaleBox from './components/SaleBox';
+import SearchBar from './components/SearchBar';
+import OptionBar from './components/OptionBar';
+import HotelListFooter from './components/HotelListFooter';
 
 const HotelListPage = () => {
   return (
-    <section css={cardContainer}>
-      {HOTEL_LIST_DATA.data.map((hotel) => (
-        <HotelListCard key={hotel.hotelId} hotel={hotel} />
-      ))}
-    </section>
+    <>
+      <Header hasBackButton={true} />
+
+      <div css={hotelListHeaderContainer}>
+        <SearchBar />
+        <OptionBar />
+      </div>
+
+      <div css={hotelListWrapper}>
+        <SaleBox />
+        <section css={cardWrapper}>
+          {HOTEL_LIST_DATA.data.map((hotel) => (
+            <HotelListCard key={hotel.hotelId} hotel={hotel} />
+          ))}
+        </section>
+      </div>
+
+      <HotelListFooter />
+    </>
   );
 };
 
