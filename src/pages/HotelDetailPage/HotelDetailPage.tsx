94b0aff5fdbe9a20d86ce205d05e11f103fe4230
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-21 15:16:54 +0900

    init: 초기세팅

--- src/pages/HotelDetailPage/HotelDetailPage.tsx
+++ src/pages/HotelDetailPage/HotelDetailPage.tsx
@@ -1,9 +1,1 @@
-import Highlights from "./Components/HotelDetail/Highlights";
-import 
-
-const HotelDetailPage = () => {
-  return <div>HotelDetailPage</div>;
-  <Highlights highlights={[]} />
-};
-
-export default HotelDetailPage;
+WebGLQuery
