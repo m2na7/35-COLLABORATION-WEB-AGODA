1b7a8f1b3c12e1c66f21716e52afbe9767d8a795
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-22 20:57:37 +0900

    feat: 시간 한정 세일 보기 기능 구현

--- src/pages/HotelListPage/HotelListPage.tsx
+++ src/pages/HotelListPage/HotelListPage.tsx
@@ -1,28 +1,42 @@
+import { useState } from 'react';
+import Header from '@components/Header/Header';
+import SaleBox from './components/SaleBox';
+import SearchBar from './components/SearchBar';
+import OptionBar from './components/OptionBar';
+import HotelListCard from './components/HotelListCard/HotelListCard';
+import HotelListFooter from './components/HotelListFooter';
+import TimeSaleSection from './components/TimeSaleSection';
 import { HOTEL_LIST_DATA } from '@utils/mocks/hotelListData';
-import HotelListCard from './components/HotelListCard';
 import {
   hotelListHeaderContainer,
   cardWrapper,
   hotelListWrapper,
 } from './HotelListPage.style';
-import Header from '@components/Header/Header';
-import SaleBox from './components/SaleBox';
-import SearchBar from './components/SearchBar';
-import OptionBar from './components/OptionBar';
-import HotelListFooter from './components/HotelListFooter';
 
 const HotelListPage = () => {
+  const [showTimeSale, setShowTimeSale] = useState(false);
+
+  const toggleTimeSale = () => {
+    setShowTimeSale((prev) => !prev);
+  };
+
   return (
     <>
       <Header hasBackButton={true} />
 
       <div css={hotelListHeaderContainer}>
         <SearchBar />
-        <OptionBar />
+        <OptionBar
+          toggleTimeSale={toggleTimeSale}
+          showTimeSale={showTimeSale}
+        />
       </div>
 
       <div css={hotelListWrapper}>
         <SaleBox />
+
+        {showTimeSale && <TimeSaleSection />}
+
         <section css={cardWrapper}>
           {HOTEL_LIST_DATA.data.map((hotel) => (
             <HotelListCard key={hotel.hotelId} hotel={hotel} />
