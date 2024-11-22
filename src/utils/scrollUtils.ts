530fb8498edd6c6b7256b72360320542232efa10
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-23 00:05:48 +0900

    fix: 스크롤 인덱스 계산 로직 수정

--- src/utils/scrollUtils.ts
+++ src/utils/scrollUtils.ts
@@ -11,6 +11,6 @@ export const calculateScrollIndex = (
   const cardGap = parseFloat(window.getComputedStyle(container).gap) || 0; // 카드 간격
   const cardOffset = cardWidth / 2 + cardGap; // 카드 너비 / 2 + 카드 간격 => dot 활성화 기준
 
-  // 인덱스가 반올림되어 변경되면 => 다음 dot 활성화
-  return Math.round(scrollPosition / cardOffset);
+  // 인덱스값이 증가하면 (0 -> 1)  => 다음 dot 활성화
+  return Math.floor(scrollPosition / cardOffset);
 };
