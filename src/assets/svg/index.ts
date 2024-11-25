4009e51d950e30a6484daea741577254e7a84cf9
Parkchaeyeon <ccyy1029@naver.com>
2024-11-26 02:14:36 +0900

    chore: 아이콘 오류, 경로 오류 수정

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -10,7 +10,6 @@ export { default as IcFilter } from "./IcFilter";
 export { default as IcLogo } from "./IcLogo";
 export { default as IcMap } from "./IcMap";
 export { default as IcMenu } from "./IcMenu";
-export { default as IcSearch } from "./IcSearch";
 export { default as IcStar } from "./IcStar";
 export { default as IcStarBlue } from "./IcStarBlue";
 export { default as TagLightred } from "./TagLightred";
