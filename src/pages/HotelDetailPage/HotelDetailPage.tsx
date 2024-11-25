352dc89deb955e2317ca65dddc5668d531a4deb9
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-11-26 02:00:32 +0900

    feat: 헤더 붙이기

--- src/pages/HotelDetailPage/HotelDetailPage.tsx
+++ src/pages/HotelDetailPage/HotelDetailPage.tsx
@@ -1,15 +1,17 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import Highlights from "./Components/HotelDetail/Highlights";
 import RateReview from "./Components/HotelDetail/RateReview";
 import Location from "./Components/HotelDetail/Location";
 import HotelCarousel from "@components/Carousel/HotelCarousel";
 import Detail from "./Components/HotelDetail/Detail";
 import TextReview from "./Components/HotelDetail/TextReview";
-//import Header from "@components/Header/Header";
+import Header from "@components/Header/Header";
 import HeaderSelection from "./Components/HotelDetail/HeaderSelection";
 
 const HotelDetailPage = () => {
   return (
-    <>
+    <Router>
+      <Header hasBackButton={true} />
       <HeaderSelection />
       <HotelCarousel />
       <Detail />
@@ -17,7 +19,7 @@ const HotelDetailPage = () => {
       <RateReview />
       <TextReview />
       <Location />
-    </>
+    </Router>
   );
 };
 
