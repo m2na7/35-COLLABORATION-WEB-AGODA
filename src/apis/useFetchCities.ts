b85e697767b191141f0c0ee75494d7fd1b6e9b0c
Parkchaeyeon <ccyy1029@naver.com>
2024-11-28 00:08:51 +0900

    feat: useFetchCities 타입 설정하고 api 연결하기

--- src/apis/useFetchCities.ts
+++ src/apis/useFetchCities.ts
@@ -3,13 +3,31 @@ import { QUERY_KEY } from "@/apis/queryKeys";
 import { useQuery } from "@tanstack/react-query";
 import { END_POINT } from "@utils/constants/api/api";
 
-const getCities = async () => {
+export type CitiesGetResponse = {
+  cities: CitiesType[];
+};
+
+export type CitiesType = {
+  cityId: number;
+  cityName: string;
+  countryName: string;
+};
+
+export interface ApiResponseType<T> {
+  code: number;
+  message: string;
+  data: T;
+}
+
+const getCities = async (): Promise<CitiesGetResponse> => {
   try {
-    const response = await get(END_POINT.GET_CITIES);
-    console.log(response);
-    return response.data;
+    const response = await get<ApiResponseType<CitiesGetResponse>>(
+      END_POINT.GET_CITIES
+    );
+    return response.data.data;
   } catch (error) {
     console.log(error);
+    throw error;
   }
 };
 
--- src/pages/CityPage/CityPage.tsx
+++ src/pages/CityPage/CityPage.tsx
@@ -5,22 +5,24 @@ import {
 } from "@pages/CityPage/CityPage.style";
 import CityCard from "@pages/CityPage/components/CityCard/CityCard";
 import CityHeader from "@pages/CityPage/components/CityHeader/CityHeader";
-import { cities } from "@utils/mocks/cities";
 
 const CityPage = () => {
   const { data } = useFetchCities();
-  console.log(data);
+
+  if (!data) {
+    return <div>도시 데이터가 없습니다.</div>;
+  }
 
   return (
     <div css={CityPageLayout}>
       <CityHeader />
       <section css={CityCardWrapper}>
-        {cities.map((cities) => {
+        {data.cities.map((city) => {
           return (
             <CityCard
-              key={cities.cityId}
-              cityName={cities.cityName}
-              countryName={cities.countryName}
+              key={city.cityId}
+              cityName={city.cityName}
+              countryName={city.countryName}
             />
           );
         })}
