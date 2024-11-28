48387556e6a9afd6a40d89580a0198b4cb493b21
Parkchaeyeon <ccyy1029@naver.com>
2024-11-28 17:03:57 +0900

    refactor: 코드리뷰 반영 (타입은 type 폴더로, 빠진 텍스트 추가, 빈 태그 제거)

--- src/pages/HotelDetailPage/components/RoomDetailCard/RoomDetailCard.tsx
+++ src/pages/HotelDetailPage/components/RoomDetailCard/RoomDetailCard.tsx
@@ -1,3 +1,4 @@
+import { RoomDataType } from "@app-types/hotelDetail";
 import { IcBath, IcBed, IcPeople, IcRoom } from "@assets/svg";
 import Carousel from "@components/Carousel/Carousel";
 import { roomDetailCardContainer } from "@pages/HotelDetailPage/components/RoomDetail/RoomDetail.style";
@@ -17,22 +18,6 @@ type RoomDetailCardProps = {
   roomData: RoomDataType;
 };
 
-type RoomDataType = {
-  roomId: number;
-  roomName: string;
-  spaceSize: number;
-  bedType: string;
-  bedCount: number;
-  bathInfo: string;
-  maxCapacity: number;
-  roomImages: RoomImageType[];
-};
-
-type RoomImageType = {
-  roomImageId: number;
-  roomImageUrl: string;
-};
-
 const RoomDetailCard = ({ roomData }: RoomDetailCardProps) => {
   const {
     roomName,
@@ -49,41 +34,39 @@ const RoomDetailCard = ({ roomData }: RoomDetailCardProps) => {
   });
 
   return (
-    <>
-      <section css={roomDetailCardLayout}>
-        <div css={roomDetailCarouselStyle}>
-          <div css={roomDetailCarouselLabelStyle}>커플/2인 여행객이 선호</div>
-          <Carousel images={hotelRoomImages} variant="round" />
-        </div>
-        <div css={roomDetailCardContainer}>
-          <span css={roomTitleStyle}>{roomName}</span>
-          <div css={roomOptionContainer}>
-            <div css={roomOptionWrapper}>
-              <div css={roomOptionStyle}>
-                <IcRoom css={roomOptionIconStyle} />
-                <span css={roomOptionTextStyle}>{spaceSize}</span>
-              </div>
-              <div css={roomOptionStyle}>
-                <IcBath css={roomOptionIconStyle} />
-                <span css={roomOptionTextStyle}>{bathInfo}</span>
-              </div>
+    <section css={roomDetailCardLayout}>
+      <div css={roomDetailCarouselStyle}>
+        <div css={roomDetailCarouselLabelStyle}>커플/2인 여행객이 선호</div>
+        <Carousel images={hotelRoomImages} variant="round" />
+      </div>
+      <div css={roomDetailCardContainer}>
+        <span css={roomTitleStyle}>{roomName}</span>
+        <div css={roomOptionContainer}>
+          <div css={roomOptionWrapper}>
+            <div css={roomOptionStyle}>
+              <IcRoom css={roomOptionIconStyle} />
+              <span css={roomOptionTextStyle}>{spaceSize}㎡</span>
             </div>
-            <div css={roomOptionWrapper}>
-              <div css={roomOptionStyle}>
-                <IcBed css={roomOptionIconStyle} />
-                <span css={roomOptionTextStyle}>
-                  {bedType} {bedCount}개
-                </span>
-              </div>
-              <div css={roomOptionStyle}>
-                <IcPeople css={roomOptionIconStyle} />
-                <span css={roomOptionTextStyle}>최대 성인 {maxCapacity}명</span>
-              </div>
+            <div css={roomOptionStyle}>
+              <IcBath css={roomOptionIconStyle} />
+              <span css={roomOptionTextStyle}>{bathInfo}</span>
+            </div>
+          </div>
+          <div css={roomOptionWrapper}>
+            <div css={roomOptionStyle}>
+              <IcBed css={roomOptionIconStyle} />
+              <span css={roomOptionTextStyle}>
+                {bedType} {bedCount}개
+              </span>
+            </div>
+            <div css={roomOptionStyle}>
+              <IcPeople css={roomOptionIconStyle} />
+              <span css={roomOptionTextStyle}>최대 성인 {maxCapacity}명</span>
             </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
